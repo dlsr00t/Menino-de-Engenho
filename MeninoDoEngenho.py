3e8ffbfc07ad60ccebb0f522e42f380cfc9c04f5
Foureyes <87150906+dlsr00t@users.noreply.github.com>
2022-10-07 18:46:33 -0300

    Add files via upload

--- MeninoDoEngenho.py
+++ MeninoDoEngenho.py
@@ -4,29 +4,29 @@ from sys import exit
 import os
 
 
-pygame.init()
+#pygame.init()
 
 largura = 640
 altura = 480
 x = largura/2
 y = altura/2
 branco = 255, 255, 255
-sprite_frente_imagem = pygame.image.load(os.path.join('assets', 'sprite_frente.png'))
-sprite_frente = pygame.transform.scale(sprite_frente_imagem, (50, 81))
 
-sprite_cima_imagem = pygame.image.load(os.path.join('assets', 'sprite_cima.png'))
-sprite_cima_imagem = pygame.transform.scale(sprite_cima_imagem, (50, 81))
+def sprite(sprite_name, largura=50, altura = 81):
+    sprite_imagem = pygame.image.load(os.path.join('assets', sprite_name+'.png'))
+    sprite_imagem = pygame.transform.scale(sprite_imagem, (largura, altura))
+    tela.fill(branco)
+    tela.blit(sprite_imagem, (x, y))
+
 
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Jogo')
 relogio = pygame.time.Clock()
-tela.fill(branco)
-tela.blit(sprite_frente, (x, y))
+relogio.tick(60)
+sprite("sprite_frente")
 
 while True:
-    relogio.tick(60)
-    #tela.fill(branco)
-    #tela.blit(sprite_frente, (x, y))
+
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -41,27 +41,19 @@ while True:
             if event.key == K_s:
                 y = y + 20'''
     if pygame.key.get_pressed()[K_a]:
-        x = x - 5
-        sprite_frente_imagem = pygame.image.load(os.path.join('assets', 'sprite_cima.png'))
-        sprite_frente_imagem = pygame.transform.scale(sprite_frente_imagem, (50, 81))
-        tela.fill(branco)
-        tela.blit(sprite_frente_imagem, (x, y))
+        x = x - 2.5
+        sprite("sprite_esquerda", largura=26)
+        
     if pygame.key.get_pressed()[K_d]:
-        x = x + 5
-        sprite_frente_imagem = pygame.image.load(os.path.join('assets', 'sprite_cima.png'))
-        sprite_frente_imagem = pygame.transform.scale(sprite_frente_imagem, (50, 81))
-        tela.fill(branco)
-        tela.blit(sprite_frente_imagem, (x, y))
+        x = x + 2.5
+        sprite("sprite_direita", largura=26)
+ 
     if pygame.key.get_pressed()[K_w]:
-        y = y - 5
-        sprite_frente_imagem = pygame.image.load(os.path.join('assets', 'sprite_cima.png'))
-        sprite_frente_imagem = pygame.transform.scale(sprite_frente_imagem, (50, 81))
-        tela.fill(branco)
-        tela.blit(sprite_frente_imagem, (x, y))
+        y = y - 4
+        sprite("sprite_cima")
+
     if pygame.key.get_pressed()[K_s]:
-        y = y + 5
-        sprite_frente_imagem = pygame.image.load(os.path.join('assets', 'sprite_frente.png'))
-        sprite_frente_imagem = pygame.transform.scale(sprite_frente_imagem, (50, 81))
-        tela.fill(branco)
-        tela.blit(sprite_frente_imagem, (x, y))
+        y = y + 4
+        sprite("sprite_frente")
+
     pygame.display.update()
