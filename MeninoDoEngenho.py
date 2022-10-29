7bd18332c4a5f870bb4b5f6ba6e0f92570c86a56
dlsr00t <dlsroot12@gmail.com>
2022-10-29 13:33:20 -0700

    29/10/2022

--- MeninoDoEngenho.py
+++ MeninoDoEngenho.py
@@ -6,37 +6,6 @@ import os
 import time
 from variaveis import *
 
-'''
-largura = 1200
-altura = 640
-branco = 255, 255, 255
-ultimo = "sprite_baixo"
-mapa = pygame.image.load(os.path.join('assets', 'mapa.png'))
-background = pygame.transform.scale(mapa, (1200, 640))
-personagem = pygame.image.load(os.path.join('assets', 'npc.png'))
-npc = pygame.transform.scale(personagem, (50, 81))
-pygame.font.init()
-fonte = pygame.font.SysFont('arial', 20, True, True)
-mensagem = ['Salve','tchau', '']
-texto_formatado = fonte.render(mensagem[0], False, (255, 255, 255))
-nome = "joao"
-nome_formatado = fonte.render(nome, False, (0,0,0))
-nada = ''
-nada_formatado = fonte.render(nome,False, (0,0,0))
-tela = pygame.display.set_mode((largura, altura))
-relogio = pygame.time.Clock()
-valor = 0
-movimentando = False
-velocity = 12
-x = largura/2
-y = altura/2
-a = 0
-b = 0
-#ret_um = pygame.Rect(x, y, 50, 81)
-#ret_dois = pygame.Rect(a + 200, b + 281, 50, 81)
-texto = False
-texto_count = 0
-'''
 
 pygame.display.set_caption('Jogo')
 
